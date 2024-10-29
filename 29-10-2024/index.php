5869e3b037fb151cfe186768b0e563c912241916
Umar/37 <umarraihan945@gmail.com>
2024-10-29 11:23:07 +0700

    Add files via upload

--- 29-10-2024/index.php
+++ 29-10-2024/index.php
@@ -1,21 +1,21 @@
 
 <?php
-$sekolah = ["sdn entalsewu", "smpn 1 sidoarjo", "smkn 2 buduran"];
-$sekolahs = ["tk" => "tk dharma wanita", "sd" => "sdn entalsewu", "smp" => "smpn 1 sidoarjo","smk" => "smkn 2 buduran", "pt" => "universitas negeri surabaya"];
+$sekolah = ["SDN Sidodadi", "SMPN 2 Sidoarjo", "SMKN 2 BUDURAN"];
+$sekolahs = ["TK" => "Tk Alkautsar", "SD" => "SDN Sidodadi", "SMP" => "SMPN 2 Sidoarjo", "SMK" => "SMKN 2 BUDURAN", "PT" => "UNIVERSITAS NEGERI SURABAYA"];
 
-$skills = ["c++" => "expert", "html" => "newbie", "css" => "intermediate", "php" => "intermediate", "javascript => intermediate"];
+$skills = ["C++" => "EXPERT", "HTML" => "NEWBIE", "CSS" => "INTERMEDIATE", "PHP" => "INTERMEDIATE", "JAVASCRIPT" => "INTERMEDIATE"];
 
-$identitas = ["nama" => "Septa Putra Ramadhan", "alamat" => "desa entalsewu rt 13 rw 04","gmail" => "septaputrar21@gmail.com"];
+$identitas = ["Nama" => "Umar Raihan Alfaridzi", "Alamat" => "Desa Jambangan Perumtas 4", "gmail" => "umarraihan945@gmail.com"]; 
 
-$hobi = ["bersepeda", "mancing"];
+$hobi = ["Ngoding", "Musik", "Olahraga"];
 
 echo $sekolah[0];
 echo "<br>";
-echo $sekolahs[tk];
+echo $sekolahs["TK"];
 echo "<br>";
 echo $sekolah[1];
 echo "<br>";
-echo $sekolahs[sd];
+echo $sekolahs["SD"];
 
 echo "<br>";
 echo "<br>";
@@ -37,10 +37,10 @@ foreach ($sekolah as $key){
 echo "<br>";
 
 foreach ($sekolahs as $key => $value){
-    echo $key;
-    echo "=";
-    echo $value;
-    echo "<br>";
+     echo $key;
+     echo "=";
+     echo $value;
+     echo "<br>";
 }
 
 echo "<br>";
@@ -82,93 +82,93 @@ if (isset($_GET["menu"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
 <body>
     <hr>
     <ul>
-        <li><a href="#">Home</a></li>   
-        <li><a href="#">CV</a></li>   
-        <li><a href="#">Project</a></li>   
-        <li><a href="#">Contact</a></li>   
+        <li><a href="#">Home</a></li>
+        <li><a href="menu = CV">CV</a></li>
+        <li><a href="menu = Project">Project</a></li>
+        <li><a href="menu = Contact">Contact</a></li>
     </ul>
     <h2>identitas</h2>
     <table border="1">
-            <thead>
-                  <tr>
-                     <th>identitas</th>
-                     <th>deskripsi</th>
-                  </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($identitas as $key => $value){
-                ?>
-                <tr>
-                   <td><?= $key ?></td>
-                   <td><?= $value ?></td>
-                </tr>
-                <?php
-            }
-            ?>
-            </tbody>
-    </table>
-    <hr>
-    <h2>Riwayat Sekolah</h2>
-    <table>
         <thead>
-             <tr>
-                <th>Jenjang</th>
-                <th>Nama Sekolah</th>
-             </tr>
+            <tr>
+                <th>identitas</th>
+                <th>deskripsi</th>
+            </tr>
         </thead>
         <tbody>
         <?php
-        foreach ($sekolahs as $key => $value){
-            echo "<tr>";
-            echo "<td>";
-            echo $key;
-            echo "</td";
-            echo "<td>";
-            echo $value;
-            echo "</td>";
-            echo "</tr>";
+        foreach ($identitas as $key => $value){
+            ?>
+            <tr>
+                <td><?= $key ?></td>
+                <td><?= $value ?></td>
+            </tr>
+            <?php
         }
         ?>
         </tbody>
     </table>
+    <hr>
     <h2>Riwayat Sekolah</h2>
-    <table>
-         <thead>
-              <tr>
-                <th>$skills</th>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Jenjang</th>
+                <th>Nama Sekolah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($sekolahs as $key => $value){
+                echo "<tr>";
+                echo "<td>";
+                echo $key;
+                echo "</td>";
+                echo "<td>";
+                echo $value;
+                echo "</td";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <h2>Riwayat Sekolah</h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Skills</th>
                 <th>Level</th>
-              </tr>
-         </thead>
-         <tbody>
-         <?php
-         foreach ($skills as $key => $value){
-             ?>
-             <tr>
-                <td><?= $key ?></td>
-                <td><?= $value ?></td>
-             </tr>
-             <?php 
-         }
-         ?>
-         </tbody>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($skills as $key => $value){
+                ?>
+                <tr>
+                    <td><?= $key ?></td>
+                    <td><?= $value ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+
+        </tbody>
     </table>
     <hr>
     <h2>hobi</h2>
     <ol>
-    <?php
-    foreach ($hobi as $key){
+        <?php
+        foreach ($hobi as $key){
+            ?>
+            <li><?= $key ?></li>
+            <?php 
+        }
         ?>
-        <li><?= $key ?></li>
-        <?php 
-    }
-    ?>
     </ol>
 </body>
 </html>

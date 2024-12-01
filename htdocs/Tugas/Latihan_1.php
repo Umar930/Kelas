eaee4227ebdf6017b0df141c128bd590e237fcc0
Umar/37 <umarraihan945@gmail.com>
2024-11-30 18:01:44 -0800

    Add files via upload

--- htdocs/Tugas/Latihan_1.php
+++ htdocs/Tugas/Latihan_1.php
@@ -1,1 +1,16 @@
+<?php
 
+    echo "Belajar PHP";
+
+    echo '<br>';
+
+    echo 'Saya Siswa SMK';
+
+    echo '<h1 style="background-color:red">Belajar PHP itu Mudah</h1>
+          <p style="background-color:blue">saya suka belajar program</p>
+    ';
+
+    echo 'Saya Belajar'.' PHP'.'<br>';
+
+    echo 2019 + 2;
+?>
